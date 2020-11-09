6c96479d2d18a16ca41d73bce89571409dc7398f
Bernardo Hummes <hummescmpa@gmail.com>
2020-11-09 13:08:17 +0100

    added a few unit tests for the td

--- tests/tests_lab.cpp
+++ tests/tests_lab.cpp
@@ -15,39 +15,49 @@
 
 #define protected public
 
-TEST_CASE("Cell")
+TEST_CASE("Cell manipulation")
 {
+    Cell *c11 = new Cell(1,1);
+    Cell *c12 = new Cell(1,2);
 
     SECTION("Test 1: Initialize Cells")
     {
-        Cell *c11 = new Cell(1,1);
         REQUIRE (c11->m_x == 1);
         REQUIRE (c11->m_y == 1);
 
-        Cell *c12 = new Cell(1,2);
         REQUIRE (c12->m_x == 1);
         REQUIRE (c12->m_y == 2);
     }
 
-//    SECTION("Test 2: Connect Cells")
-//    {
-//        REQUIRE (c11->m_nb_neighb == 0);
-//        Cell::add_neighb(c11,c12);
-//        REQUIRE (c11->m_nb_neighb == 1);
-//    }
-//
-//    SECTION("Test 3: Connects Cells")
-//    {
-//        display_graph(c21);  // testing only if function doesn't result in errors
-//                             // visual testing done separately
-//    }
-//
-//    SECTION("Test 4: Display Graph")
-//    {
-//        display_graph(c21);  // testing only if function doesn't result in errors
-//                             // visual testing done separately
-//    }
+    SECTION("Test 2: Connect Cells")
+    {
+        REQUIRE (c11->m_nb_neighb == 0);
+        Cell::add_neighb(c11,c12);
+        REQUIRE (c11->m_nb_neighb == 1);
+    }
 
+    SECTION("Test 3: Connect Cells")
+    {
+        Cell *c11 = new Cell(1,1);
+        Cell *c12 = new Cell(1,2);
+
+        REQUIRE (c11->m_nb_neighb == 0);
+        c11->add_neighb(c12);
+        REQUIRE (c11->m_nb_neighb == 1);
+
+    }
+}
+
+TEST_CASE("Cell display")
+{
+    Cell *c11 = new Cell(1,1);
+
+    SECTION("Test 1: toString")
+    {
+        std::string expected = "(1,1)";
+
+        REQUIRE(expected.compare(c11->toString()));
+    }
 }
 
 //TEST_CASE("Path")
